e1574ebebadb44c7778573868647d0d9cbd39da8
Karthik Jain <krthkj.public@gmail.com>
2025-11-20 19:31:22 +0530

    optimized layers and allow variations and versioning

--- build.sh
+++ build.sh
@@ -3,8 +3,24 @@
 # docker build -t tag:ver -f Dockerfile .
 # docker run --rm -it --gpus all tag:ver
 
+# Export the variable, creating a unique timestamp
+export BUILD_VERSION=$(date +%Y%m%d-%H%M)
+
+## Build images 
 cd amd64
 docker compose build
-sleep 5s
-docker compose push
 cd ..
+sleep 2s
+
+## Push images to docker hub
+docker images | grep krthkj/cpp | awk '{print $1":"$2}' | xargs -I {} docker push {}
+echo -e "\033[32mPushed docker images to dockerhub\033[0m"
+sleep 2s
+
+## remove the docker hub tags after pushing
+docker images | grep krthkj/cpp | awk '{print $1":"$2}' | xargs -I {} docker rmi {}
+echo -e "\033[32muntagged docker images that were pushed to dockerhub\033[0m"
+
+sleep 2s
+echo -e "\033[34mImages with timestamp as tags\033[0m"
+docker images | awk '$2 ~ /^[0-9]{8}-[0-9]{4}$/ {print $1":"$2}'
