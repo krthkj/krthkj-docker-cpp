a079d21ae0ad30de707290d355dd6f6a212144ad
Karthik Jain <krthkj.public@gmail.com>
2025-11-29 23:32:29 +0530

    feat: enhance compiler toolchain and image management - Add CC/CXX exports and update-alternatives for cc/c++ symlinks - Enable edge, clang20, and cuda Docker Compose services - Add automatic cleanup of timestamped images older than 1 day

--- build.sh
+++ build.sh
@@ -20,7 +20,38 @@ sleep 2s
 ## remove the docker hub tags after pushing
 docker images | grep krthkj/cpp | awk '{print $1":"$2}' | xargs -I {} docker rmi {}
 echo -e "\033[32muntagged docker images that were pushed to dockerhub\033[0m"
-
 sleep 2s
+
+## display all images with timestamp tags
 echo -e "\033[34mImages with timestamp as tags\033[0m"
 docker images | awk '$2 ~ /^[0-9]{8}-[0-9]{4}$/ {print $1":"$2}'
+sleep 2s
+
+## Delete all images with timestamp tags older than 1 day
+TIME_1_DAY_AGO=$(date +%s -d "1 day ago")
+echo -e "\033[34mFinding ALL timestamp tags created OLDER than 1 day ago...\033[0m"
+docker images --format '{{.Repository}}:{{.Tag}} {{.CreatedAt}}' | \
+awk -v T_CUTOFF="${TIME_1_DAY_AGO}" '
+{
+    IMAGE_TAG = $1
+    TAG = $1
+    # Remove the repository part to isolate the tag for checking
+    sub(/.*:/, "", TAG)
+    
+    CREATED_AT_SEC = $2
+    
+    # 1. Check if the tag is a timestamp (YYYYMMDD-HHMM)
+    if (TAG ~ /^[0-9]{8}-[0-9]{4}$/) {
+        # 2. Check if the creation time is LESS THAN the cutoff time (meaning it is OLDER than 1 day ago)
+        if (CREATED_AT_SEC < T_CUTOFF) {
+            print IMAGE_TAG
+        }
+    }
+}' | xargs -r docker rmi 
+
+if [ $? -eq 0 ]; then
+    echo -e "\n\033[32mSuccessfully deleted timestamp tags older than 1 day for ALL repos.\033[0m"
+else
+    echo -e "\n\033[31mError during tag deletion or no matching tags found.\033[0m"
+fi
+docker image prune -a --filter "until=24h" -f
--- prune.sh
+++ prune.sh
@@ -4,10 +4,13 @@
 docker image prune -f
 
 # removes all unused images including dangiling and images not refecenced by any container
-#docker image prune -a -f
+# docker image prune -a -f
 
-# Removed all unused images created more than 7 days ago nad dangling images
-docker image prune -a --filter "until=168h" -f
+# Removed all unused images created more than 1 days ago and dangling images
+docker image prune -a --filter "until=24h" -f
+
+## Purge all the tags which has timestamp
+# docker images --format "{{.Repository}}:{{.Tag}}" | grep -E ":[0-9]{8}-[0-9]{4}$" | xargs -r docker rmi
 
 ## Removes all stopped containers, all networks not used by at least one container, all dangling images,
 ## and all unused images (not just dangling ones). 
@@ -18,3 +21,37 @@ docker image prune -a --filter "until=168h" -f
 ## Performs the cleanup above AND removes all unused volumes.
 ## WARNING: Volumes contain data! Use this command only if you are certain the data in unused volumes is not needed.
 #docker system prune -a --volumes -f
+
+# --- CONFIGURATION ---
+# Calculate the single cutoff time in seconds since the epoch
+# TIME_1_DAY_AGO: Any image created BEFORE this time (i.e., less than this number) is older than 1 day.
+TIME_1_DAY_AGO=$(date +%s -d "1 day ago") 
+# ---------------------
+
+echo ""
+echo -e "\033[34mFinding ALL timestamp tags created OLDER than 1 day ago...\033[0m"
+# Find images created older than 1 day and delete the corresponding tags
+docker images --format '{{.Repository}}:{{.Tag}} {{.CreatedAt}}' | \
+awk -v T_CUTOFF="${TIME_1_DAY_AGO}" '
+{
+    IMAGE_TAG = $1
+    TAG = $1
+    # Remove the repository part to isolate the tag for checking
+    sub(/.*:/, "", TAG)
+    
+    CREATED_AT_SEC = $2
+    
+    # 1. Check if the tag is a timestamp (YYYYMMDD-HHMM)
+    if (TAG ~ /^[0-9]{8}-[0-9]{4}$/) {
+        # 2. Check if the creation time is LESS THAN the cutoff time (meaning it is OLDER than 1 day ago)
+        if (CREATED_AT_SEC < T_CUTOFF) {
+            print IMAGE_TAG
+        }
+    }
+}' | xargs -r docker rmi 
+
+if [ $? -eq 0 ]; then
+    echo -e "\n\033[32mSuccessfully deleted timestamp tags older than 1 day for ALL repos.\033[0m"
+else
+    echo -e "\n\033[31mError during tag deletion or no matching tags found.\033[0m"
+fi
